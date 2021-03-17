08b9b1e6a504fe5468a9e329b0bb30179da41ad7
kmisiag <37901082+kmisiag@users.noreply.github.com>
2021-03-17 14:56:24 +0100

    Add Package.swift (#1)
    Co-authored-by: Kamil Misiąg <kmisiag@speednet.pl>

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RxQuery",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "RxQuery", targets: ["RxQuery"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.0.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.0")),
+    ],
+    targets: [
+        .target(name: "RxQuery", dependencies: ["Alamofire", "RxSwift", "RxCocoa"], path: "RxQuery")
+    ]
+)
